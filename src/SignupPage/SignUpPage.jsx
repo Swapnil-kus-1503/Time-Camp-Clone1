f6c326e4ef261487815c84e67f6e7a353ce7de47
jstgrowup <deysubham999@gmail.com>
2022-09-27 19:03:56 +0530

    working on the signup FE

---
+++ src/SignupPage/SignUpPage.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import SignupNavbar from './SignupNavbar'
+
+function SignUpPage() {
+    return (
+        <Box>
+            <SignupNavbar />
+
+        </Box>
+    )
+}
+
+export default SignUpPage
---
+++ src/SignupPage/SignupLeftCompo.jsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+function SignupLeftCompo() {
+    return (
+        <Box>
+          <Text></Text>
+        </Box>
+    )
+}
+
+export default SignupLeftCompo
