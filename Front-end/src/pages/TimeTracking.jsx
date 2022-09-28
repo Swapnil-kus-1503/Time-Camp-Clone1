a670d379d4256d6902c39c4a8afa7591b5f7e508
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-09-28 11:47:13 +0000

    Added: Section two and demo CTA🔥🔥🔥

--- Front-end/src/pages/TimeTracking.jsx
+++ Front-end/src/pages/TimeTracking.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Top from "../components/timeTracker/Top";
 import SocialProof from "../components/timeTracker/SocialProof";
+import DemoCta from "../components/timeTracker/DemoCta";
+import TopHeader from "../components/timeTracker/TopHeader";
+import FeatureSection from "../components/timeTracker/FeatureSection";
 
 const TimeTracking = () => {
   return (
     <>
       <Top />
       <SocialProof />
+      <DemoCta />
+      <TopHeader />
+      {/* features-perks */}
+      <FeatureSection />
     </>
   );
 };
