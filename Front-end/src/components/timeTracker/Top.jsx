fc054c7d1d75c832d39c51a6fa3dcab57602d533
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-09-28 10:25:20 +0000

    Added: Top Component ✨🚀

---
+++ Front-end/src/components/timeTracker/Top.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import "./css/features.css";
+
+const Top = () => {
+  return (
+    <section id='top'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-12 features-top'>
+            <div className='wrapper'>
+              <h1 className='heading-features'>
+                Time tracker your team will actually use
+              </h1>
+              <p>
+                Track time against your projects and create reports and
+                timesheets in seconds.
+              </p>
+              <div className='form'>
+                <form className='inline-form-tc'>
+                  <div className='form-wrapper'>
+                    <div className='button-wrapper'>
+                      <input
+                        type='email'
+                        name='email'
+                        placeholder='Add Your Email'
+                      />
+                      <button className='btn-filled green form-home-bottom'>
+                        Start tracking time
+                      </button>
+                      <div className='button-wrapper__google'>
+                        <span>Or sign up with: </span>
+                        <a href='/'>
+                          <img
+                            src='https://cdn-m.timecamp.com/img/logos/google.svg'
+                            alt=''
+                          />
+                        </a>
+                        <p className='terms-submission'>
+                          <label>
+                            By signing up you agree to our
+                            <a
+                              href='/terms-conditions/'
+                              rel='nofollow noopener noreferrer'
+                              target='_blank'
+                            >
+                              Terms and Conditions
+                            </a>
+                            and
+                            <a
+                              href='/privacy-policy/'
+                              rel='nofollow noopener noreferrer'
+                              target='_blank'
+                            >
+                              Privacy Policy
+                            </a>
+                            .
+                          </label>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className='wrapper'>
+              <div className='image-content'>
+                <img
+                  src='https://cdn-m.timecamp.com/img/greenbranding/features/features-time-tracking.svg'
+                  alt='features time tacking timesheets'
+                ></img>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Top;
--- Front-end/src/pages/TimeTracking.jsx
+++ Front-end/src/pages/TimeTracking.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Top from "../components/timeTracker/Top";
 
 const TimeTracking = () => {
-  return <div>TimeTracking</div>;
+  return (
+    <>
+      <Top></Top>
+    </>
+  );
 };
 
 export default TimeTracking;
