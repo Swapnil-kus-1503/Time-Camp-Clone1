d16a908476c661a2c576eee80c67e06bbe2c05d0
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-09-28 10:50:12 +0000

    Added: Social Proof and CSS ✌✌✌

--- Front-end/src/pages/TimeTracking.jsx
+++ Front-end/src/pages/TimeTracking.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Top from "../components/timeTracker/Top";
+import SocialProof from "../components/timeTracker/SocialProof";
 
 const TimeTracking = () => {
   return (
     <>
-      <Top></Top>
+      <Top />
+      <SocialProof />
     </>
   );
 };
