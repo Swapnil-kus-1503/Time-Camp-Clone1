d05d7e7153a111a8eb638af9f25e82b450aaa18f
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-09-28 11:47:13 +0000

    Added: Section two and demo CTA🔥🔥🔥

---
+++ Front-end/src/components/timeTracker/TopHeader.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TopHeader = () => {
+  return (
+    <section id='top-header'>
+      <div class='container'>
+        <div class='row'>
+          <div class='col-sm-12'>
+            <h2>One app. A multitude of benefits</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TopHeader;
