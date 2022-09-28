fddd19ef79359078d8dbb60c7878b60368a1ebb6
jstgrowup <deysubham999@gmail.com>
2022-09-28 19:27:36 +0530

    chrome button in login fixed

--- Front-end/src/LoginPage/LoginLeftCompo.jsx
+++ Front-end/src/LoginPage/LoginLeftCompo.jsx
@@ -1,8 +1,8 @@
-import { Box,  Center, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react'
+import { Box, Center, Flex, Heading, Image, Stack, Text, Button } from '@chakra-ui/react'
 import React from 'react'
 
 function LoginLeftCompo() {
-   
+
     return (
         <Flex rowGap={"10"} direction="column" justify={"center"}>
             <Text color={"#25CF60"} >PRODUCT UPDATE</Text>
@@ -26,16 +26,16 @@ function LoginLeftCompo() {
                 </Heading>
             </Box>
             {/* PROBLEM */}
-            {/* <Button w={"96"} fontSize={"md"} color={"white"} size={"lg"} bg={"#25CF60"} borderRadius={"3xl"} >
+            <Button w={["70", "96"]} fontSize={"md"} color={"white"} size={"lg"} bg={"#25CF60"} borderRadius={"3xl"} >
                 <Image maxH={"7"} src="https://cdn.timecamp.com/res/css/images/chrome-icon-button.1664279108.png"></Image> Add TimeCamp for Chrome
-            </Button> */}
+            </Button>
             {/* PROBLEM */}
             <Center>
                 <Stack spacing={["3", "5", "7", "10"]} direction={["column", "column", "row", "row"]}>
-                    <Image h={["24","16", "14"]} w={["40", "45","36", "36"]} src='https://cdn.timecamp.com/res/css/images/crozdesk-icon.1664279108.png'></Image>
-                    <Image h={["24","16", "14"]} w={["40", "45","36", "36"]} src='https://cdn.timecamp.com/res/css/images/capterra-icon.1664279108.png'></Image>
-                    <Image h={["24","16", "14"]} w={["40", "45","36", "36"]} src='https://cdn.timecamp.com/res/css/images/crowd-icon.1664279108.png'></Image>
-                    <Image h={["24","16", "14"]} w={["40", "45","36", "36"]} src='https://cdn.timecamp.com/res/css/images/get-app-icon.1664279108.png'></Image>
+                    <Image h={["24", "16", "14"]} w={["40", "45", "36", "36"]} src='https://cdn.timecamp.com/res/css/images/crozdesk-icon.1664279108.png'></Image>
+                    <Image h={["24", "16", "14"]} w={["40", "45", "36", "36"]} src='https://cdn.timecamp.com/res/css/images/capterra-icon.1664279108.png'></Image>
+                    <Image h={["24", "16", "14"]} w={["40", "45", "36", "36"]} src='https://cdn.timecamp.com/res/css/images/crowd-icon.1664279108.png'></Image>
+                    <Image h={["24", "16", "14"]} w={["40", "45", "36", "36"]} src='https://cdn.timecamp.com/res/css/images/get-app-icon.1664279108.png'></Image>
                 </Stack>
             </Center>
 
