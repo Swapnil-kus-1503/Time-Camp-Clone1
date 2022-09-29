8665a699234fa2826dbe72bbf2f8507abcc3ba4c
Swapnil-kus-1503 <ashukushwaha15031997@gmail.com>
2022-09-29 22:19:22 +0530

    final day-2

--- Front-end/src/components/timeTracker/DemoCta.jsx
+++ Front-end/src/components/timeTracker/DemoCta.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const DemoCta = () => {
   return (
-    <section id='demo-cta'>
-      <div class='container'>
-        <div class='row'>
+    <section id="demo-cta">
+      <div class="container">
+        <div class="row">
           <img
-            src='https://cdn-m.timecamp.com/img/greenbranding/greenSmallSkewCircle.svg'
-            alt='decorating'
-            class='greenSmallSkewCircle'
+            src="https://cdn-m.timecamp.com/img/greenbranding/greenSmallSkewCircle.svg"
+            alt="decorating"
+            class="greenSmallSkewCircle"
           />
           <img
-            src='https://cdn-m.timecamp.com/img/greenbranding/redSmallCircle.svg'
-            alt='decorating'
-            class='redSmallCircle'
+            src="https://cdn-m.timecamp.com/img/greenbranding/redSmallCircle.svg"
+            alt="decorating"
+            class="redSmallCircle"
           />
-          <div class='col-sm-12'>
-            <div class='text'>
+          <div class="col-sm-12">
+            <div class="text">
               <h2>Schedule a demo and learn more about TimeCamp</h2>
               <p>
                 Hop on a call with TimeCamp's top minds to get the best possible
@@ -24,22 +24,22 @@ const DemoCta = () => {
               </p>
             </div>
             <a
-              class='nav-link btn-filled yellow demo-cta-hop'
-              href='/'
-              title='sign up for free time tracker'
+              class="nav-link btn-filled yellow demo-cta-hop"
+              href="/"
+              title="sign up for free time tracker"
             >
               Book a demo
             </a>
           </div>
           <img
-            src='https://cdn-m.timecamp.com/img/greenbranding/blueSmallCircle.svg'
-            alt='decorating'
-            class='blueSmallCircle'
+            src="https://cdn-m.timecamp.com/img/greenbranding/blueSmallCircle.svg"
+            alt="decorating"
+            class="blueSmallCircle"
           />
           <img
-            src='https://cdn-m.timecamp.com/img/greenbranding/greenSmallCircle.svg'
-            alt='decorating'
-            class='greenSmallCircle'
+            src="https://cdn-m.timecamp.com/img/greenbranding/greenSmallCircle.svg"
+            alt="decorating"
+            class="greenSmallCircle"
           />
         </div>
       </div>
--- Front-end/src/components/timeTracker/TopHeader.jsx
+++ Front-end/src/components/timeTracker/TopHeader.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const TopHeader = () => {
   return (
-    <section id='top-header'>
-      <div class='container'>
-        <div class='row'>
-          <div class='col-sm-12'>
+    <section id="top-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
             <h2>One app. A multitude of benefits</h2>
           </div>
         </div>
