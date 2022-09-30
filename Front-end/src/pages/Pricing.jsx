20f8df1b80193df3ed94e53d5349ef7fb664d9b2
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-09-30 20:35:08 +0000

    Added: Top ✨

--- Front-end/src/pages/Pricing.jsx
+++ Front-end/src/pages/Pricing.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Top from "../components/pricing/Top";
 import Navbar from "../HomeCompo/Navbar/Navbar";
+import "../components/pricing/css/price.css";
 
 const Pricing = () => {
   return (
-    <>
+    <div className="pricing">
       <Navbar />
-    </>
+      <Top />
+    </div>
   );
 };
 
