ab96ae705d6dfb7351d7ac1dc21d19995bc766be
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-09-30 21:08:42 +0000

    Added: Customer Story 🔥

---
+++ Front-end/src/components/pricing/CustomerStory.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const CustomerStory = () => {
+  return (
+    <section id="customer-story">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2>What customers say</h2>
+          </div>
+          <div class="col-md-12 col-lg-3 story">
+            <h2>Improving your records every day</h2>
+            <p>
+              "Being able to measure the time each task takes or each activity
+              that takes place during the day, brings control to your time and
+              improves your own records to grow professionally every day more."
+            </p>
+            <div class="wrapper">
+              <div class="text">
+                <p>
+                  <span>Felipe</span>
+                </p>
+                <p>Software Developer</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12 col-lg-3 story">
+            <h2>A huge asset for our remote business</h2>
+            <p>
+              "TimeCamp was a recent implementation for us after using a
+              different time tracking system. It is a clean easy to use software
+              with barely any learning curve. It has everything we need, min by
+              min tracking, notes, projects, tasks, multiple reports, etc. It
+              has been a huge asset to our remote business"
+            </p>
+            <div class="wrapper">
+              <div class="text">
+                <p>
+                  <span>Michelle</span>
+                </p>
+                <p>Recruiting Director</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12 col-lg-3 story">
+            <h2>We can distribute workload in a fair basis</h2>
+            <p>
+              "With TimeCamp we are capable to understand exactly which part of
+              the project require more time so we can find a solution and also
+              the saturation of the team so we can distribute workload in a fair
+              basis."
+            </p>
+            <div class="wrapper">
+              <div class="text">
+                <p>
+                  <span>Capterra user</span>
+                </p>
+                <p>Freelancer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CustomerStory;
