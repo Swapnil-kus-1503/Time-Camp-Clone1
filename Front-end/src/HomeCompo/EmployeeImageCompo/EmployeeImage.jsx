0c07593600492aa276bc46aaf01eb6416fd906f4
Swapnil-kus-1503 <ashukushwaha15031997@gmail.com>
2022-09-30 11:53:38 +0530

    homepage

--- Front-end/src/HomeCompo/EmployeeImageCompo/EmployeeImage.jsx
+++ Front-end/src/HomeCompo/EmployeeImageCompo/EmployeeImage.jsx
@@ -13,101 +13,23 @@ import {
 function EmployeeImage() {
   return (
     <div>
-      
-      <Box bg={"#25cf60"} pt={"20px"}>
-        <Box w={"100%"} minH={"1px"}>
-          <Heading color={"white"} fontWeight={"700"} textAlign={"center"}>
-            Trusted by 18,000 teams from all over the world (and still growing!)
-          </Heading>
-        </Box>
-
-        <Flex
-          pb={"0.5rem"}
-          justify={"space-between"}
-          direction={{ base: "column", sm: "column", md: "column", lg: "row" }}
-          w={{ base: "100%", sm: "100%", md: "100%", lg: "80%" }}
-        >
-          <Box
-            textAlign={"center"}
-            display="flex"
-            justifyContent={"center"}
-            position={"relative"}
-            minH="1px"
-          >
-            <Image
-              borderStyle={"none"}
-              src="https://cdn-m.timecamp.com/img/greenbranding/social-proof-section/usp-logo-white.png"
-              alt=""
-            />
-          </Box>
-
-          <Box
-            textAlign={"center"}
-            display="flex"
-            justifyContent={"center"}
-            position={"relative"}
-            minH="1px"
-          >
-            <Image
-              borderStyle={"none"}
-              src="https://cdn-m.timecamp.com/img/greenbranding/social-proof-section/dsv-logo-white.png"
-              alt=""
-            />
-          </Box>
-
-          <Box
-            textAlign={"center"}
-            display="flex"
-            justifyContent={"center"}
-            position={"relative"}
-            minH="1px"
-          >
-            <Image
-              borderStyle={"none"}
-              src="https://cdn-m.timecamp.com/img/greenbranding/social-proof-section/raindrop-logo-white.png"
-            />
-          </Box>
-          <Box
-            textAlign={"center"}
-            display="flex"
-            justifyContent={"center"}
-            position={"relative"}
-            minH="1px"
-          >
-            <Image
-              borderStyle={"none"}
-              src="https://cdn-m.timecamp.com/img/greenbranding/social-proof-section/saatchi-logo-white.png"
-              alt=""
-            />
-          </Box>
-          <Box
-            textAlign={"center"}
-            display="flex"
-            justifyContent={"center"}
-            position={"relative"}
-            minH="1px"
-          >
-            <Image
-              borderStyle={"none"}
-              src="https://cdn-m.timecamp.com/img/greenbranding/social-proof-section/blitz-logo-white.png"
-              alt=""
-            />
-          </Box>
-          <Box
-            textAlign={"center"}
-            display="flex"
-            justifyContent={"center"}
-            position={"relative"}
-            minH="1px"
-          >
-            <Image
-              borderStyle={"none"}
-              src="https://cdn-m.timecamp.com/img/greenbranding/social-proof-section/havas-logo-white.png"
-              alt=""
-            />
-          </Box>
-        </Flex>
+      <Box
+        overflowX={"hidden"}
+        bgSize={"cover"}
+        bgPosition={"center"}
+        bgRepeat={"no-repeat"}
+        p={"6rem"}
+        height={"1100px"}
+        border="2px"
+        bgImg={"https://cdn-m.timecamp.com/img/backgroundTop.webp"}
+      >
+        <Center>
+          {/* write everything here  */}
+          <Flex bgColor={"black"} w={"60%"} h={"300px"} border={"2px"}></Flex>
+          {/* write everything here */}
+        </Center>
       </Box>
+
       <Center>
         <SimpleGrid
           columns={["1", "1", "2"]}
