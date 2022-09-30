59d3d9892335d4cb5b3460ae24f0c3e6dddecb96
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-09-30 21:08:42 +0000

    Added: Customer Story 🔥

--- Front-end/src/pages/Pricing.jsx
+++ Front-end/src/pages/Pricing.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Top from "../components/pricing/Top";
 import Navbar from "../HomeCompo/Navbar/Navbar";
 import "../components/pricing/css/price.css";
+import YourTimeCostsMore from "../HomeCompo/YourTimeCostsMore/YourTimeCostsMore";
+import SocialProof from "../components/timeTracker/SocialProof";
+import BottomCta from "../components/timeTracker/BottomCta";
+import CustomerStory from "../components/pricing/CustomerStory";
 
 const Pricing = () => {
   return (
     <div className="pricing">
       <Navbar />
       <Top />
+      <YourTimeCostsMore />
+      <SocialProof />
+      <BottomCta />
+      <CustomerStory />
     </div>
   );
 };
