97657809984e8f43fb77e85235fdde9837b27e3e
jstgrowup <deysubham999@gmail.com>
2022-10-01 20:25:47 +0530

    minor changes

--- Front-end/src/LoginPage/LoginNavbar.jsx
+++ Front-end/src/LoginPage/LoginNavbar.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 function LoginNavbar() {
   const navigate = useNavigate();
   return (
-    <Box minH={"80px"} boxShadow={"rgba(0, 0, 0, 0.1) 0px 4px 12px"}>
+    <Box minH={"80px"} boxShadow={"rgba(0, 0, 0, 0.1) 0px 4px 12px"} onClick={() => navigate("/")}>
       <Image
         onClick={() => navigate("/")}
         cursor={"pointer"}
