71f568169d0860fe0b1db7bce33954137caf4418
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-10-01 07:43:30 +0000

    Fix: CSS 💅

---
+++ Front-end/src/Data/featuresData.js
@@ -0,0 +1,49 @@
+export const dataa = {
+  "automatic-time-tracking": {
+    headingFeatures: "Automatic time tracking",
+    headingFeaturesp:
+      "Harness TimeCamp's advanced automation. All you need to do is turn on the app and let it do all the work for you.",
+    TopImg:
+      "https://cdn-m.timecamp.com/img/greenbranding/features/automatic-time-tracking-header.svg",
+    QuickLook: {
+      one: "One-click time tracker",
+      oneDes:
+        "Fully automated time tracker that keeps track so you can focus more on your work.",
+      two: "App integrations",
+      twoDes:
+        "Integrate your work software with TimeCamp and maintain your current workflow",
+      three: "Graphic timesheet",
+      threeDes:
+        "Add, adjust and delete time entries in an easy and readable way.",
+      four: "Keyword based tracking",
+      fourDes:
+        "Setup task and project-specific keywords so that you won't have to switch between projects manually.",
+    },
+    features:{
+        one: "One-click tracker",
+        oneDes:
+          "lets you track whenever you need to automatically switches to idle when not working turning on and off with just one click",
+        oneImg:"https://cdn-m.timecamp.com/img/greenbranding/easyofuse.png",
+        two: "App integrations",
+        twoDes:
+          "Whether you're managing a software project, a group of consultants or handling your client's books, TimeCamp is capable of integrating the most popular work apps so that you can maintain your current workflow.",
+        twoImg:"https://cdn-m.timecamp.com/img/greenbranding/integrations.png",
+        three: "Graphic timesheets",
+        threeDes:
+          "If you're thinking that adding time entries involves going through multiple tabs and entering some special code - you're wrong. An easy drag and drop interface will allow adding entries with no hassle.",
+        threeImg:"https://cdn-m.timecamp.com/img/greenbranding/graphicTimesheet.png",
+        four: "Keyword-based tracking",
+        fourDes:
+        "If you're switching between tasks and whole projects, measuring time spent can be quite a challenge. That's why you can set up task and project-specific keywords so that TimeCamp will automatically switch between projects so you don't have to.",
+        fourImg:"https://cdn-m.timecamp.com/img/greenbranding/keyword.png",
+    },
+    steps:{
+        one:"76M",
+        oneDes:"Total hours tracked",
+        two:"6",
+        twoDes:"Ways to track time",
+        three:"1",
+        threeDes:"Click to track time",
+    }
+  },
+};
